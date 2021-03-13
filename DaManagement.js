bd5392801a21dd6e5b3e3595cae0a62652c83597
Adam Reynolds <github@smufflersworld.com>
2021-03-13 12:10:05 +0000

    renamed

--- DaManagement.js
+++ DaManagement.js
@@ -22,7 +22,7 @@ var howLongStage = 60;
 						 (only work when queue = true)
 						*/
 
-global.masterIds = ['604056db3f4bfc001be4c22f']; //example (clear this before using)
+global.masterIds = ['']; //example (clear this before using)
 /*This is the master id list, userid's that are put in here will not be affected by the song length limit, artist / song banning, the /skip command, or the dj afk limit.
 						 This is meant to explicitly give extra privileges to yourself and anyone else you want to put in here. It takes userid's as input in string format separated by commas.
 						 You can put the person's name in the array either before or after a userid to tell who it belongs to, it will not affect its ability to function.
