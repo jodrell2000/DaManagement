5979719991766d8b13aa827632b2981742d0485f
Adam Reynolds <adam.reynolds@oracle.com>
2021-04-06 21:03:12 +0100

    - adding a chat specific auth

---
+++ authChat.js
@@ -0,0 +1,8 @@
+// use this bookmarklet to find the IDs for your user 7 ROOM
+// http://alaingilbert.github.io/Turntable-API/bookmarklet.html
+
+module.exports = {
+    AUTH: 'xxx', //set the auth of your bot here.
+    USERID: 'xxx', //set the userid of your bot here.
+    ROOMID: 'xxx' //set the roomid of the room you want the bot to go to here.
+}
