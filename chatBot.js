4395d9dd2c2e3207167acb00c7caa9cc1ed522ee
Ben Griffiths <bengriffiths@gmail.com>
2021-04-08 21:18:09 +0100

    Capture bot by function closure in chatModule

--- chatBot.js
+++ chatBot.js
@@ -1,21 +1,21 @@
-let authModule = require('./authChat.js');
-let chatModule = require('./chatbot/chatModule.js');
+let authModule = require("./authChat.js");
+let chatResponder = require("./chatbot/chatModule.js");
 
-let Bot = require('ttapi');
+let Bot = require("ttapi");
 let bot = new Bot(authModule.AUTH, authModule.USERID, authModule.ROOMID);
 
 bot.debug = true;
 
-bot.on('newsong', function () {
+bot.on("newsong", function () {
   bot.bop();
 });
 
-bot.on('speak', function (data) {
+const responder = chatResponder(bot);
+
+bot.on("speak", function (data) {
   if (data.text.match(/^\//)) {
-    chatModule.parseText(bot, data);
+    responder.parseText(data);
   }
 });
 
-setInterval(function () {
-    chatModule.tickTok(bot)
-}, 5000);
+setInterval(responder.tickTok, 5000);
--- chatbot/chatModule.js
+++ chatbot/chatModule.js
@@ -1,38 +1,38 @@
-let responseModule = require('../chatbot/responseModule.js');
-let thisMsgText = 'Hey! How are you @';
+let responseModule = require("../chatbot/responseModule.js");
+let thisMsgText = "Hey! How are you @";
 
-module.exports = {
-    parseText, buildMessage, tickTok,
-}
+const saySomething = (bot) => {
+  responseModule.responseCount++;
+  bot.speak(responseModule.responseCount + ": " + message);
+};
 
-function saySomething (bot, message) {
-    responseModule.responseCount ++;
-    bot.speak(responseModule.responseCount+': '+message);
-}
+const buildMessage = (name) => {
+  return thisMsgText + name;
+};
 
-function tickTok (bot) {
-    switch (responseModule.responseCount%2) {
+const chatResponder = (bot) => {
+  return {
+    tickTok: () => {
+      switch (responseModule.responseCount % 2) {
         case 0:
-            saySomething(bot,"tik");
-            break;
-
+          saySomething(bot, "tik");
+          break;
         default:
-            saySomething(bot,"tok");
+          saySomething(bot, "tok");
+      }
+    },
 
-    }
-}
+    parseText: (data) => {
+      // Get the data
+      let name = data.name;
+      let text = data.text;
 
-function parseText (bot, data) {
-    // Get the data
-    let name = data.name;
-    let text = data.text;
+      // Respond to "/hello" command
+      if (text.match(/^\/hello$/)) {
+        saySomething(buildMessage(name));
+      }
+    },
+  };
+};
 
-    // Respond to "/hello" command
-    if (text.match(/^\/hello$/)) {
-        saySomething(bot, buildMessage(name));
-    }
-}
-
-function buildMessage (name) {
-    return thisMsgText+name;
-}
+module.exports = chatResponder;
