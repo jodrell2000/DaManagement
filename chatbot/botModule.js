8a6dd332327adc5e5631c783c1f5a36b8aabee77
Adam Reynolds <adam.reynolds@oracle.com>
2021-06-23 17:34:12 +0100

    Trying to make the help command work, which means parsing arguments out of the set commands and using them...not there yet

--- chatbot/botModule.js
+++ chatbot/botModule.js
@@ -2,8 +2,36 @@ let responseModule = require("../chatbot/responseModule.js");
 
 let chatModule = require("../chatbot/chatModule.js");
 
+const availableCommands = {};
+
 const botModule = (bot) => {
     const responder = chatModule(bot);
+
+    availableCommands.hello = (data) => {
+        responder.sayHello(data.name);
+    }
+    availableCommands.hello.help = "'/hello': Reply to the sender with a Hello! Likely used to test if the Bot is working";
+
+    availableCommands.list = () => {
+        responder.saySomething("Available commands are: " + listCommands());
+    }
+    availableCommands.list.help = "'/list': Lists all available commands";
+
+    availableCommands.help = () => {
+        displayHelp(command);
+    }
+    availableCommands.help.arguments = (1);
+    availableCommands.help.help = "/help [command] Display how to use an individual command";
+
+    function displayHelp(command) {
+        responder.saySomething(availableCommands[command].help);
+    }
+
+    function listCommands() {
+        console.log("Listing all available commands");
+        console.log("Commands:" + Object.keys(availableCommands));
+        return Object.keys(availableCommands);
+    }
 
     return {
         tikTok: () => {
@@ -25,11 +53,30 @@ const botModule = (bot) => {
             }
         },
 
-        getCommandFromText: function (theText) {
-            const firstSpace = theText.indexOf(" ");
-            const commandWord = theText.substring( 1, firstSpace );
+        getArguments: function (theText) {
+            let startIndex = theText.indexOf(" ");
+            let remainingText = theText.substring(startIndex, theText.length);
 
-            return commandWord;
+            if (startIndex !== remainingText.indexOf(" ")) {
+                console.log("Should have found a space for an argument here");
+                return theCommand;
+            }
+            for (let argumentLoop = 0; argumentLoop < availableCommands[theCommand].arguments; argumentLoop++) {
+                theCommand.argument[argumentLoop] = remainingText.substring(startIndex, remainingText.length);
+                remainingText = remainingText.substring(startIndex, remainingText.length);
+            }
+        },
+
+        getCommandFromText: function (theText) {
+            let theCommand;
+            const firstSpace = theText.indexOf(" ");
+            if (firstSpace !== -1) {
+                theCommand = theText.substring(1, firstSpace);
+            } else {
+                theCommand = theText.substring(1, theText.length);
+            }
+
+            return theCommand;
         },
 
         newParseCommands: function (data, userFunctions) {
@@ -42,17 +89,19 @@ const botModule = (bot) => {
             const moderatorCommands = {};
             //moderatorCommands.riggedflip = () => { action: "riggedFlipResponse", helpText: "Ask the bot to flip a coin for you, always tails" }
 
-            const availableCommands = {};
-            availableCommands.hello = () => { responder.sayHello(data.name); }
-            //availableCommands.coinflip = () => { action: "coinFlipResponse", helpText: "Ask the bot to flip a coin for you" }
-
-            let command;
+            let theCommand;
             //const isMod = userFunctions.isUserModerator(speaker) === true;
 
-            if ((command = availableCommands[sentCommand])) {
-                command();
-            //} else if ((command = moderatorCommands[sentCommand]) && isMod) {
-            //    command();
+            console.log("sentCommand: " + sentCommand);
+
+            if ((theCommand = availableCommands[sentCommand])) {
+                if (availableCommands[theCommand].hasOwnProperty('arguments')) {
+                    this.getArguments(sentCommand)
+                } else {
+                    theCommand(data);
+                }
+                //} else if ((command = moderatorCommands[sentCommand]) && isMod) {
+                //    command();
             } else {
                 bot.speak("No idea what you're talking about");
             }
@@ -60,11 +109,11 @@ const botModule = (bot) => {
 
         parseCommand: function (data) {
             let theCommand = data.text;
-            console.log("parseCommand, theCommand:" + theCommand + "-----" );
+            console.log("parseCommand, theCommand:" + theCommand + "-----");
 
-            for (let commandLoop = 0; commandLoop < availableCommands.length; commandLoop++ ) {
-                console.log("In the commandLoop:" + commandLoop + "-----" );
-                if ( availableCommands[commandLoop]["command"] === theCommand ) {
+            for (let commandLoop = 0; commandLoop < availableCommands.length; commandLoop++) {
+                console.log("In the commandLoop:" + commandLoop + "-----");
+                if (availableCommands[commandLoop]["command"] === theCommand) {
                     responder[availableCommands[commandLoop]["action"]](data);
                 }
             }
