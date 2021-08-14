ade258c436dda92c4819f482f840104689c70ecc
Ben Griffiths <bengriffiths@gmail.com>
2021-08-14 21:44:16 +0100

    A few more tweaks to tidy up the module

--- modules/videoModule.js
+++ modules/videoModule.js
@@ -5,6 +5,7 @@ let authorize = require("./oauth2lib");
 let { setIntersection, setDifference } = require("../modules/setlib");
 
 let SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"];
+let CLIENT_SECRET_PATH = "client_secret.json";
 let TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH) + "/.credentials/";
 let TOKEN_PATH = TOKEN_DIR + "theManagementCredentials.json";
 
@@ -56,51 +57,34 @@ const videoFunctions = () => {
 
   return {
     listAlertRegions: function (data, chatFunctions) {
-      chatFunctions.botSpeak(
-        data,
-        `The list of regions that will triger a blocked alert is currently ${Array.from(
-          regionsWeCareAbout
-        )}`
-      );
+      const regionsAsArray = Array.from(regionsWeCareAbout);
+      const message = `The list of regions that will triger a blocked alert is currently ${regionsAsArray}`;
+      chatFunctions.botSpeak(data, message);
     },
 
-    addAlertRegion: function (data, args, chatFunctions) {
-      const region = args[0];
+    addAlertRegion: function (data, [region], chatFunctions) {
+      let message;
       if (regionsWeCareAbout.has(region)) {
-        chatFunctions.botSpeak(
-          data,
-          `${region} is already in the region alerts list`
-        );
+        message = `${region} is already in the region alerts list`;
       } else {
         regionsWeCareAbout.add(region);
-        chatFunctions.botSpeak(
-          data,
-          `${region} has been added to the region alerts list`
-        );
+        message = `${region} has been added to the region alerts list`;
       }
+      chatFunctions.botSpeak(data, message);
     },
 
-    removeAlertRegion: function (data, args, chatFunctions) {
-      const region = args[0];
-      const removed = regionsWeCareAbout.delete(region);
-      if (!removed) {
-        chatFunctions.botSpeak(
-          data,
-          `${region} is not in the region alerts list`
-        );
+    removeAlertRegion: function (data, [region], chatFunctions) {
+      let message;
+      if (regionsWeCareAbout.delete(region)) {
+        message = `${region} has been removed from the region alerts list`;
       } else {
-        chatFunctions.botSpeak(
-          data,
-          `${region} has been removed from the region alerts list`
-        );
+        message = `${region} is not in the region alerts list`;
       }
+      chatFunctions.botSpeak(data, message);
     },
 
-    readRegions: function (data, args, userFunctions, chatFunctions) {
-      // const theDJID = userFunctions.getCurrentDJID()
-      const videoID = args[0];
-
-      authorize("client_secret.json", TOKEN_PATH, SCOPES)
+    readRegions: function (data, [videoID], userFunctions, chatFunctions) {
+      authorize(CLIENT_SECRET_PATH, TOKEN_PATH, SCOPES)
         .then((oauthClient) => getRegionRestrictions(oauthClient, videoID))
         .then((restrictions) => {
           if (restrictions.allowed !== undefined) {
