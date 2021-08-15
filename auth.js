1da2c1c167d006d3e25fd2247a04980bc9541dd0
Adam Reynolds <adam.reynolds@oracle.com>
2021-08-15 14:55:08 +0100

    USERID, ROOMID, AUTH and the commandSwitch are now stored in environment variables

--- auth.js
+++ auth.js
@@ -1,13 +1,9 @@
-// use this bookmarklet to find the IDs for your user 7 ROOM
-// http://alaingilbert.github.io/Turntable-API/bookmarklet.html
+// configured using environment variables in a .env file
+// copy the .env.example file to .env and fill ouut the details for your Bot user
+const dotenv = require('dotenv').config()
 
 module.exports = {
-    AUTH: 'XXX', //set the auth of your bot here.
-    USERID: 'XXXX', //set the userid of your bot here.
-
-    // 70s, 80s & 90s Awesomesauce
-    // ROOMID: '604ca44347b5e3001a8feb25' //set the roomid of the room you want the bot to go to here.
-
-    // I <3 Th 80s
-    ROOMID: 'XXXX' //set the roomid of the room you want the bot to go to here.
+    AUTH: process.env.AUTH,
+    USERID: process.env.USERID,
+    ROOMID: process.env.ROOMID
 }
--- defaultSettings/chatDefaults.js
+++ defaultSettings/chatDefaults.js
@@ -1,3 +1,8 @@
+// change the switch uused to identify commands sent to yout bot in your .env file
+// copy the .env.example file to .env and fill ouut the details for your Bot user
+
+const dotenv = require('dotenv')
+
 module.exports = {
-    commandIdentifier: "!",
+    commandIdentifier: process.env.COMMANDIDENTIFIER,
 }
