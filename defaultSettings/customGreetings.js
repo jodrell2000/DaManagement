12a00ba1dc2f9996a4d1c7f75ef38f29d493901d
Adam Reynolds <github@smufflersworld.com>
2021-09-02 11:18:32 +0100

    add custom welcome message for @GirlfriendInAComa

--- defaultSettings/customGreetings.js
+++ defaultSettings/customGreetings.js
@@ -3,6 +3,8 @@
 
 module.exports = {
     // personal greeting messages
-    userGreetings: [ { id: "6040a0333f4bfc001be4cf39", username: "jodrell", message: "Ahhhh @jodrell, welcome back...and will Sir be playing any Marillion today?" }
+    userGreetings: [
+        { id: "6040a0333f4bfc001be4cf39", username: "jodrell", message: "Ahhhh @jodrell, welcome back...and will Sir be playing any Marillion today?" },
+        { id: "6042311dc2dbd9001be7872f", username: "GirlfriendInAComa", message: "I know, I know, it's seriously time to welcome @GirlfriendInAComa to the room!" }
     ],
 }
