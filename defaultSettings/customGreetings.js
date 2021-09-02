01666a56725c6453ade93131e90ef5dfcaf75358
Adam Reynolds <github@smufflersworld.com>
2021-09-02 18:32:16 +0100

    Management welcome message

--- defaultSettings/customGreetings.js
+++ defaultSettings/customGreetings.js
@@ -5,6 +5,7 @@ module.exports = {
     // personal greeting messages
     userGreetings: [
         { id: "6040a0333f4bfc001be4cf39", username: "jodrell", message: "Ahhhh @jodrell, welcome back...and will Sir be playing any Marillion today?" },
+        { id: "6044d40847c69b001e447957", username: "The Management", message: "oh...it's just The Management, ignore him. A mere prototype" },
         { id: "604068d23f4bfc001be4c698", username: "Kelsi", message: "Who released the Bats? Oh just @Kelsi" },
         { id: "604154083f4bfc001c3a42ed", username: "BukkakeBlaster", message: "PUT UP THE TARPS!!! @BukkakeBlaster has arrived ;-)" },
         { id: "6042311dc2dbd9001be7872f", username: "GirlfriendInAComa", message: "I know, I know, it's seriously time to welcome @GirlfriendInAComa to the room!" }
