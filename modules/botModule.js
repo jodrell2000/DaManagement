c6fadfa5d636e0dc33c6e44d77d4f28b7e842509
Adam Reynolds <github@smufflersworld.com>
2023-04-15 16:22:08 +0100

    .

--- modules/botModule.js
+++ modules/botModule.js
@@ -281,8 +281,11 @@ const botFunctions = ( bot ) => {
 
         async isFavouriteArtist ( databaseFunctions, theArtist ) {
             console.group( "botModule:isFavouriteArtist" );
-            const artistList = await databaseFunctions.getVerifiedArtistsFromName( theArtist );
-            console.log( "aartistList:" + artistList );
+            const artistList = await databaseFunctions.getVerifiedArtistsFromName( theArtist )
+                .then( () => {
+                    console.log( "aartistList:" + artistList );
+
+                } )
             console.groupEnd();
         },
 
