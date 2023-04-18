197b8e947c1e664748d3af9fed37a0f4491ffbf1
Adam Reynolds <github@smufflersworld.com>
2023-04-18 18:44:02 +0100

    .

--- scripts/dbBackup.sh
+++ scripts/dbBackup.sh
@@ -16,7 +16,7 @@ current_datetime=$(date +%Y-%m-%d-%H-%M-%S)
 one_month_ago=$(date -d "1 month ago" +%Y-%m-%d)
 
 # Run mysqldump to backup the database
-mysqldump -u $DBUSERNAME -p$DBPASSWORD $db_name > $backup_dir/$db_name-$current_date.sql
+mysqldump -u $BACKUPDBUSERNAME -p$DBPASSWORD $db_name > $backup_dir/$db_name-$current_date.sql
 
 # Delete backups that are older than one month
 find $backup_dir -name "$db_name-*.sql" -type f -mtime +30 -exec rm {} \;
