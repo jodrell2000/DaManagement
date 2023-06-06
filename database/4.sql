dbe948ceaaa78c94af454939f06c9d5ed9452480
Adam Reynolds <github@smufflersworld.com>
2023-06-06 12:13:23 +0100

    one more column

--- database/4.sql
+++ database/4.sql
@@ -2,4 +2,5 @@ USE robotoDB;
 
 ALTER TABLE tracksPlayed 
 MODIFY COLUMN `upvotes` int NOT NULL DEFAULT '0', 
-MODIFY COLUMN `downvotes` int NOT NULL DEFAULT '0';
+MODIFY COLUMN `downvotes` int NOT NULL DEFAULT '0',
+MODIFY COLUMN `snags` int NOT NULL DEFAULT '0';
