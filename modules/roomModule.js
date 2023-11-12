1a836e7f434f88514a6e6054e687ec0a3d9af60e
Adam Reynolds <github@smufflersworld.com>
2023-11-12 14:57:33 +0000

    .

--- modules/roomModule.js
+++ modules/roomModule.js
@@ -402,10 +402,10 @@ const roomFunctions = ( bot ) => {
 
         clearSongLimitTimer ( userFunctions, roomFunctions ) {
             //this is for the song length limit
-            console.log( "this.songLimitTimer:" + this.songLimitTimer )
+            console.log( "this.songLimitTimer:" + this.songLimitTimer() )
             console.log( "songLimitTimer:" + songLimitTimer )
-            if ( this.songLimitTimer !== null ) {
-                clearTimeout( this.songLimitTimer );
+            if ( this.songLimitTimer() !== null ) {
+                clearTimeout( this.songLimitTimer() );
                 this.songLimitTimer = null;
 
                 if ( typeof userFunctions.getUsername( roomFunctions.lastdj() ) !== 'undefined' ) {
