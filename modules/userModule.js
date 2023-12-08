97c42ef79eadeafc6d2c1022e0da0a9bfccbe342
Adam Reynolds <github@smufflersworld.com>
2023-12-08 16:40:17 +0000

    add another userID to the masterIDs list

--- modules/userModule.js
+++ modules/userModule.js
@@ -23,7 +23,9 @@ let vipList = [];
 /* this is the vip list, it accepts userids as input, this is for when you have a special guest or guests in your room and you only
    want to hear them dj, leave this empty unless you want everyone other than the people whos userids are in the vip list to be automatically kicked from stage. */
 
-let masterIds = [ '6040a0333f4bfc001be4cf39' ]; //example (clear this before using) 
+let masterIds = [ '6040a0333f4bfc001be4cf39', '604068d23f4bfc001be4c698' ]; //example (clear this before using) 
+// jodrell: 6040a0333f4bfc001be4cf39
+// Kelsi: 604068d23f4bfc001be4c698
 /*  This is the master id list, userid's that are put in here will not be affected by the song length limit, artist / song banning, the /skip command, or the dj afk limit.
     This is meant to explicitly give extra privileges to yourself and anyone else you want to put in here. It takes userid's as input in string format separated by commas.
     You can put the person's name in the array either before or after a userid to tell who it belongs to, it will not affect its ability to function. */
