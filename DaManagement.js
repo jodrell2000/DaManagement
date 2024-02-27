4a978510255860ca15cf3d7d28a3970ca44d4870
Adam Reynolds <github@smufflersworld.com>
2024-02-27 16:14:57 +0000

    Fix signup page

--- DaManagement.js
+++ DaManagement.js
@@ -776,7 +776,6 @@ app.post( '/signup', async ( req, res, next ) => {
     console.group( "signup" );
     const { email, username, password, confirmPassword } = req.body;
     const userID = await userFunctions.getUserIDFromUsername( username );
-    return res.status( 500 ).send( 'Dummy error' );
 
     try {
         if ( password !== confirmPassword ) {
