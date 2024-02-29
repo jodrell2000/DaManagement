0d26708285b58328dc11db918346096617b376ef
Adam Reynolds <github@smufflersworld.com>
2024-02-29 18:21:04 +0000

    Convert aliases

--- scripts/convertAliases.sh
+++ scripts/convertAliases.sh
@@ -41,6 +41,7 @@ parse_aliases() {
     for command_data in $commands; do
       while IFS=$'\n' read -r alias; do
           command_id=$(get_command_id chatCommands command "$command_data")
+          echo "command ID: $command_id"
           echo mysql --login-path=local $DBNAME -e "INSERT INTO chatAliases (command_id, alias) VALUES ($command_id, '$(escape_single_quotes "$alias")');"
       done < <(jq -r --arg cmd "$command_data" '.commands[$cmd]' $JSON_FILE)
     done
