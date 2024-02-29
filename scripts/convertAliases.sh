97bc740c501c291c46e76c98dd4acdfd5be98c8a
Adam Reynolds <github@smufflersworld.com>
2024-02-29 18:14:08 +0000

    Convert aliases

--- scripts/convertAliases.sh
+++ scripts/convertAliases.sh
@@ -19,23 +19,29 @@ parse_and_insert() {
     echo mysql --login-path=local $DBNAME -e "$sql"
 }
 
+get_command_id() {
+    local command="$1"
+
+    # Construct SQL query with placeholders
+    local sql
+    sql="SELECT id FROM chatCommands WHERE command = '$command';"
+
+    # Execute SQL query using MySQL client
+    echo mysql --login-path=local $DBNAME -e "$sql"
+}
+
 # Parse chatMessages data
 parse_aliases() {
     local aliases
     commands=$(jq -r '.commands' "$JSON_FILE")
     echo "commands: ${commands[0]}"
-    # Loop through each alias
-    while IFS= read -r line; do
-        local alias
-        alias=$(echo "$line" | jq -r 'keys[]')
-        echo "alias: $alias"
-        local command
-        command=$(echo "$line" | jq -r '.[]')
-        echo "command: $command"
-
-        # Insert alias and command into database
-        parse_and_insert chatAliases "$alias" "$command"
-    done <<<"$aliases"
+    
+    for command_data in $commands; do
+      while IFS=$'\n' read -r alias; do
+          command_id=$(get_command_id chatCommands command "$command_data")
+          echo mysql --login-path=local $DBNAME -e "INSERT INTO chatAliases (command_id, alias) VALUES ($command_id, '$(escape_single_quotes "$alias")');"
+      done < <(jq -r --arg cmd "$command_data" '.commands[$cmd]' $JSON_FILE)
+    done
 }
 
 # Main function
