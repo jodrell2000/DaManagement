bc4470bc402afadf621bdd3a713a7d96380e1af4
Adam Reynolds <github@smufflersworld.com>
2024-03-01 11:15:26 +0000

    Convert aliases

--- scripts/convertAliases.sh
+++ scripts/convertAliases.sh
@@ -5,20 +5,6 @@ DBNAME="robotoDB"
 # JSON file containing the data
 JSON_FILE="../data/aliases.json"
 
-# Parse JSON data and insert into database
-parse_and_insert() {
-    local table="$1"
-    local key="$2"
-    local value="$3"
-
-    # Construct SQL query with placeholders
-    local sql
-    sql="INSERT INTO $table ($key) VALUES ('$value');"
-
-    # Execute SQL query using MySQL client
-    echo mysql --login-path=local $DBNAME -e "$sql"
-}
-
 get_command_id() {
     local table="$1"
     local command="$2"
@@ -26,10 +12,12 @@ get_command_id() {
     # Construct SQL query with placeholders
     local sql
     sql="SELECT id FROM $table WHERE command = '$command';"
-
+    echo "SQL Query: $sql"
+    
     # Execute SQL query using MySQL client
     local id
     id=$(mysql --login-path=local $DBNAME -e "$sql" -sN)
+    echo "Command ID: $id"  # Add this line for debugging purposes
     echo "$id"
 
 }
